bcbcf022e835def280cceac5a4755511102814ec
Adrian Rodriguez <adrianwritenow@gmail.com>
2017-08-05 16:53:04 -0400

    compare is working must clean up thpugh

--- app.js
+++ app.js
@@ -2,16 +2,25 @@ const express = require('express');
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-
 const expressValidator = require('express-validator');
 const fs = require('fs');
 const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
 
 
+
 var app = express();
+var userGuess = [];
+var gameArray = [];
 
 let randomWord = words[Math.floor((Math.random() * 235000))
-].split('')
+].split('');
+randomWord.forEach(function(letter){
+  gameArray.push(" ");
+
+});
+
+
+
 //sets a session
 
 app.use(session({
@@ -37,25 +46,84 @@ app.use(bodyParser.urlencoded({
 
 //config Express Validator
 app.use(expressValidator());
-app.get('/', function (request,response){
-  if (! request.session) {
-    response.redirect('/game');
-  }else{
-    response.render('game');
-    console.log(randomWord);
 
-  }
+app.get('/', function (request,response){
+    response.render('game',{
+      userEntrey: userGuess,
+      randomWord: gameArray
+
+  });
+    console.log(randomWord);
+    console.log(userGuess);
+    console.log(gameArray);
+  });
+
+
+app.post('/', function(request, response) {
+  var schema = {
+    'userEntrey': {
+      notEmpty: true,
+      isLength: {
+        options: {
+          max: 1
+        },
+        errorMessage: 'One letter at a time'
+      },
+      errorMessage: 'Please enter a letter'
+    }
+
+  };
+  console.log('received letter from form: ' + request.body.userEntrey);
+  request.assert(schema);
+  request.getValidationResult().then(function(results) {
+    if (results.isEmpty()) {
+      newGuess = {
+        guess : request.body.userEntrey
+      }
+      console.log(newGuess.guess, "is the newGuess");
+      userGuess.push(newGuess);
+      console.log(userGuess);
+
+      randomWord.forEach(function(letter){
+        if (letter === newGuess.guess) {
+          console.log('good guess')
+        }else {
+          console.log('try again')
+        }
+      });
+
+
+
+      response.render('game', {
+        userEntrey: userGuess,
+        randomWord: gameArray
+
+
+      });
+      let count =  userGuess.length;
+      let guessLeft = 8 - count;
+      console.log(count + "guesses");
+      console.log(guessLeft);
+      if (guessLeft === 0) {
+        console.log('gameOver');
+
+      }
+
+    } else {
+      response.render('game', {
+        userEntrey: userGuess,
+        randomWord: gameArray,
+        errorMessage: results.array()
+      });
+    }
+
+  });
+
 
 
 });
+
 
 app.listen(3000, function(){
   console.log('Server farted');
 });
-
-// app.get('/', function (response, request){
-//   if (request.session) {
-//
-//   }
-
-// });
